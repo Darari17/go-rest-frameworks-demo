f538c26db920ae03434eff74f1671b07a6feb2b5
farid <faridramadhan59@gmail.com>
2025-05-29 06:32:27 +0700

    [go gin - still on progress]

--- gin/internal/repositories/post_repo.go
+++ gin/internal/repositories/post_repo.go
@@ -1,6 +1,8 @@
 package repositories
 
 import (
+	"errors"
+
 	"github.com/Darari17/go-rest-frameworks-demo/gin/internal/models"
 	"gorm.io/gorm"
 )
@@ -22,4 +24,75 @@ func NewPostRepo(db *gorm.DB) PostRepo {
 	return &postRepo{
 		db: db,
 	}
+}
+
+// CreatePost implements PostRepo.
+func (p *postRepo) CreatePost(post *models.Post) (*models.Post, error) {
+
+	if err := p.db.Create(post).Error; err != nil {
+		return nil, err
+	}
+	return post, nil
+}
+
+// DeletePostByPostID implements PostRepo.
+func (p *postRepo) DeletePostByPostID(postID uint) error {
+
+	result := p.db.Delete(&models.Post{}, postID)
+	if result.Error != nil {
+		return result.Error
+	}
+	if result.RowsAffected == 0 {
+		return errors.New("post not found")
+	}
+	return nil
+}
+
+// GetPostByPostID implements PostRepo.
+func (p *postRepo) GetPostByPostID(postID uint) (*models.Post, error) {
+
+	var post models.Post
+	if err := p.db.Preload("User").First(&post, postID).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, errors.New("post not found")
+		}
+		return nil, err
+	}
+	return &post, nil
+}
+
+// GetPostsByUserID implements PostRepo.
+func (p *postRepo) GetPostsByUserID(userID uint) ([]*models.Post, error) {
+
+	var posts []*models.Post
+	if err := p.db.Preload("User").Find(&posts).Error; err != nil {
+		return nil, err
+	}
+	return posts, nil
+}
+
+// UpdatePost implements PostRepo.
+func (p *postRepo) UpdatePost(post *models.Post) (*models.Post, error) {
+
+	// cek user id dan post id
+	var existingPost models.Post
+	if err := p.db.Where("id = ? AND user_id = ?", post.ID, post.UserID).First(&existingPost).Error; err != nil {
+		return nil, errors.New("post not found")
+	}
+
+	// update yang pilih saja
+	err := p.db.Model(&existingPost).
+		Select("Content", "ImageURL", "UpdatedAt").
+		Updates(post).
+		Error
+	if err != nil {
+		return nil, err
+	}
+
+	// get data terbaru
+	if err := p.db.Preload("User").First(&existingPost, post.ID).Error; err != nil {
+		return nil, err
+	}
+
+	return &existingPost, nil
 }
--- gin/internal/repositories/user_repo.go
+++ gin/internal/repositories/user_repo.go
@@ -19,3 +19,20 @@ func NewUserRepo(db *gorm.DB) UserRepo {
 		db: db,
 	}
 }
+
+// Create implements UserRepo.
+func (u *userRepo) Create(user *models.User) (*models.User, error) {
+	if err := u.db.Create(user).Error; err != nil {
+		return nil, err
+	}
+	return user, nil
+}
+
+// GetEmailOrUsername implements UserRepo.
+func (u *userRepo) GetEmailOrUsername(req string) (*models.User, error) {
+	var user models.User
+	if err := u.db.Where("email = ? OR username = ?", req, req).First(&user).Error; err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
