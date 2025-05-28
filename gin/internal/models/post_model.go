93b27cc86590e3b93d9cd590f386b067bacf842c
farid <faridramadhan59@gmail.com>
2025-05-28 22:46:33 +0700

    [go gin - still on progress]

---
+++ gin/internal/models/post_model.go
@@ -0,0 +1,13 @@
+package models
+
+import "time"
+
+type Post struct {
+	ID        uint      `gorm:"primaryKey"`
+	UserID    uint      `gorm:"not null"`
+	Content   string    `gorm:"type:text;not null"`
+	ImageURL  *string   `gorm:"type:text"`
+	CreatedAt time.Time `gorm:"autoCreateTime"`
+	UpdatedAt time.Time `gorm:"autoUpdateTime"`
+	User      User      `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+}
