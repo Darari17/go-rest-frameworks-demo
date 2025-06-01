0f93634960592df2e3c40bccd6e4eedfa2297495
farid <faridramadhan59@gmail.com>
2025-06-01 20:20:46 +0700

    [go gin - still on progress]

--- gin/internal/services/post_service.go
+++ gin/internal/services/post_service.go
@@ -1,1 +1,24 @@
 package services
+
+import (
+	"github.com/Darari17/go-rest-frameworks-demo/gin/internal/jwt"
+	"github.com/Darari17/go-rest-frameworks-demo/gin/internal/repositories"
+)
+
+type PostService struct {
+	postRepo   repositories.PostRepo
+	jwtHandler jwt.JWTHandler
+}
+
+func NewPostService(postRepo repositories.PostRepo, jwtHandler jwt.JWTHandler) *PostService {
+	return &PostService{
+		postRepo:   postRepo,
+		jwtHandler: jwtHandler,
+	}
+}
+
+// func (ps *PostService) CreatePost()
+// func (ps *PostService) DeletePost()
+// func (ps *PostService) UpdatePost()
+// func (ps *PostService) GetPostByPostID()
+// func (ps *PostService) GetPostsByUserID()
